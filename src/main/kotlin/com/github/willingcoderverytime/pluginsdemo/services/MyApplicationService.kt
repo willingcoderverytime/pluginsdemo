fa87af10f5713dde2ae1659eb8ba768853f0f762
GitHub Action <action@github.com>
2022-06-30 09:18:33 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/willingcoderverytime/pluginsdemo/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.willingcoderverytime.pluginsdemo.services
+
+import com.github.willingcoderverytime.pluginsdemo.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
